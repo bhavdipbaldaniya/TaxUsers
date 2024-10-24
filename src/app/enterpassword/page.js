77d2fca1a42ab232830d07f818f8f5205b3d3d45
bhavdipbaldaniya <109936913+bhavdipbaldaniya@users.noreply.github.com>
2024-10-24 10:16:52 +0530

    Update page.js

--- src/app/enterpassword/page.js
+++ src/app/enterpassword/page.js
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import EnterPassword from '../login/Component/Enterpassword'
+import EnterPassword from '../login/Component/EnterPassword'
 
 const page = () => {
     return (
